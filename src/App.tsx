b096d982eee1cc52c1b7b1a775cbafdb46922f94
Luke Bangs <bangsluke@gmail.com>
2023-03-21 07:24:15 +0000

    p52 React Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,46 @@
 import "./App.css";
 
-const list = [
-	{
-		title: "React",
-		url: "https://reactjs.org/",
-		author: "Jordan Walke",
-		num_comments: 3,
-		points: 4,
-		objectID: 0,
-	},
-	{
-		title: "Redux",
-		url: "https://redux.js.org/",
-		author: "Dan Abramov, Andrew Clark",
-		num_comments: 2,
-		points: 5,
-		objectID: 1,
-	},
-];
+type Story = {
+	objectID: number;
+	url: string;
+	title: string;
+	author: string;
+	num_comments: number;
+	points: number;
+};
+
+type Stories = Story[];
 
 const App = () => {
+	const stories = [
+		{
+			title: "React",
+			url: "https://reactjs.org/",
+			author: "Jordan Walke",
+			num_comments: 3,
+			points: 4,
+			objectID: 0,
+		},
+		{
+			title: "Redux",
+			url: "https://redux.js.org/",
+			author: "Dan Abramov, Andrew Clark",
+			num_comments: 2,
+			points: 5,
+			objectID: 1,
+		},
+	];
+
 	return (
 		<div>
 			<h1>My Hacker Stories</h1>
+
 			<Search />
 
 			<hr />
-			<List />
+
+			<List list={stories} />
 		</div>
-	);
-};
-
-export default App;
-
-const List = () => {
-	return (
-		<ul>
-			{list.map(function (item) {
-				return (
-					<li key={item.objectID}>
-						<span>
-							<a href={item.url}>{item.title}</a>
-						</span>
-						<span>{item.author}</span>
-						<span>{item.num_comments}</span>
-						<span>{item.points}</span>
-					</li>
-				);
-			})}
-		</ul>
 	);
 };
 
@@ -67,3 +59,32 @@ const Search = () => {
 		</div>
 	);
 };
+
+type ListProps = {
+	list: Stories;
+};
+
+const List: React.FC<ListProps> = (props) => (
+	<ul>
+		{props.list.map((item) => (
+			<Item key={item.objectID} item={item} />
+		))}
+	</ul>
+);
+
+type ItemProps = {
+	item: Story;
+};
+
+const Item: React.FC<ItemProps> = (props) => (
+	<li>
+		<span>
+			<a href={props.item.url}>{props.item.title}</a>
+		</span>
+		<span>{props.item.author}</span>
+		<span>{props.item.num_comments}</span>
+		<span>{props.item.points}</span>
+	</li>
+);
+
+export default App;
