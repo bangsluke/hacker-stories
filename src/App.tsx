4d81c44cf9d6e91691f4b13f37516ee325ec6908
Luke Bangs <bangsluke@gmail.com>
2023-03-21 07:32:16 +0000

    p67 React Controlled Components

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => {
 		},
 	];
 
-	const [searchTerm, setSearchTerm] = React.useState("");
+	const [searchTerm, setSearchTerm] = React.useState("React");
 
 	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchTerm(event.target.value);
@@ -44,7 +44,7 @@ const App = () => {
 		<div>
 			<h1>My Hacker Stories</h1>
 
-			<Search onSearch={handleSearch} />
+			<Search search={searchTerm} onSearch={handleSearch} />
 
 			<hr />
 
@@ -54,13 +54,14 @@ const App = () => {
 };
 
 type SearchProps = {
+	search: string;
 	onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const Search: React.FC<SearchProps> = (props) => (
 	<div>
 		<label htmlFor='search'>Search: </label>
-		<input id='search' type='text' onChange={props.onSearch} />
+		<input id='search' type='text' value={props.search} onChange={props.onSearch} />
 	</div>
 );
 
